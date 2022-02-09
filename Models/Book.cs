ef599e3ed3e5c1f851150273bdcdab4fe21cdd57
Jakub Faliszewski <jfaliszewski@interia.pl>
2022-02-09 19:12:59 +0100

    BookForm validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,16 +9,19 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Please provide correct book name")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Please provide correct author name")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Please select genre")]
 		public Genre Genre { get; set; }
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "Please provide valid date")]
 		public DateTime ReleaseDate { get; set; }
+		[Range(1, 20, ErrorMessage = "Number of stock must be bettwen 1 and 20")]
+		[Required(ErrorMessage = "Please provide a number of books")]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
